489227fe2a9a86d668a2ea03e8bb4af1af85c5fb
donbourne <donbourne1@gmail.com>
2023-12-03 21:29:40 -0500

    cleaned up day3

--- day3.py
+++ day3.py
@@ -1,49 +1,4 @@
-'''
-part 1
-
-read the input into a numpy ndarray arr_1
-pad arr_1 with '_' by maximum possible size of part number (np.pad)
-create boolean value mask mask_is_number indicating location of numbers
-create boolean value mask m_1 indicating location of symbols
-create boolean value mask mask_is_symbol_adjacent indicating location of cells adjacent to symbols
-create mask m_actual of numbers (mask_is_number) that are overlapped by mask_is_symbol_adjacent
-
-add m_actual to mask_part_number
-while m_actual isn't empty
-  create mask m_possible by shifting mask m_actual one to the left (np.roll)
-  create mask m_actual of numbers (mask_is_number) that are overlapped by m_possible
-  add m_actual to mask_part_number
-
-do same thing again (starting from mask m_actual of numbers (mask_is_number) that are overlapped by mask_isSymbolAdjacent), but shift right
-
-convert rows of m_partnums to strings
-split strings by spaces to get numbers
-sum numbers
-'''
-
-'''
-part 2
-
-for each * symbol on the board
-    create a new version of the board with just that one * symbol, and all the numbers
-    modify part 1 to count the number of numbers left on the board when non-adjacent numbers are removed
-    for symbols with 2 numbers remaining on the board, calculate their product and sum the products
-'''
-
 import numpy as np
-
-
-def print_ndarray(arr):
-    for x in arr.tolist():
-        for y in x:
-            if arr.dtype == 'bool':
-                if y:
-                    print('X', sep=' ', end='')
-                else:
-                    print('_', sep=' ', end='')
-            else:
-                print(y, sep=' ', end='')
-        print()
 
 
 def find_part_numbers(arr_1):
@@ -98,6 +53,7 @@ np.set_printoptions(threshold=10000, linewidth=100000)
 with open('day3.txt', 'rt') as f:
     lines = [list(line.strip()) for line in f.readlines()]
 
+# add padding around the board to avoid having to deal with edge conditions when boxing around symbols
 arr_1 = np.array(lines)
 arr_1 = np.pad(arr_1, pad_width=1, mode='constant', constant_values='_')
 
@@ -106,13 +62,13 @@ _, total, _ = find_part_numbers(arr_1)
 print(f'total: {total}')
 
 # part 2
-# find all of the * symbols
+# find the * symbols
 star_symbols = np.nonzero(arr_1 == '*')
 star_symbols = [(row, col) for row, col in zip(star_symbols[0], star_symbols[1])]
 keep_numbers = np.frompyfunc(lambda x: x if '0' <= x <= '9' else ' ', 1, 1)
 arr_numbers = keep_numbers(arr_1)
 
-# add one * symbol to the number array at a time and check the product of adjacent numbers
+# add one * symbol to the number array at a time and check the product of any adjacent numbers
 total = 0
 for star_symbol in star_symbols:
     # add star_symbol to number array
